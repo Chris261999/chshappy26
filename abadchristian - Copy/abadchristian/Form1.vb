b102eed80b3f7de007a4e64f746600d11ede7323
Chris261999 <135182703+Chris261999@users.noreply.github.com>
2023-06-06 10:15:24 +0800

    Add files via upload

---
+++ abadchristian - Copy/abadchristian/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If My.Settings.LoadCount < 0 Then
+            Form2.ShowDialog()
+        End If
+        Me.Text = My.Settings.appstat & ": count = > " & My.Settings.LoadCount
+        My.Settings.LoadCount -= 1
+        My.Settings.Save()
+    End Sub
+End Class
---
+++ abadchristian - Copy/abadchristian/Form2.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Cha24XX
+        If My.Settings.Serialkey = TextBox1.Text Then
+            My.Settings.appstat = "full version"
+            My.Settings.Save()
+            Close()
+        Else
+            MsgBox("invalid serialkey")
+        End If
+    End Sub
+End Class
